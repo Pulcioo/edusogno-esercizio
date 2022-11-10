f6750456ae09663b5891395e4a60a15bca9ca466
Davide Pulci <davide.pulci@gmail.com>
2022-11-10 14:31:13 +0100

    password hashata

--- php/register.php
+++ php/register.php
@@ -6,8 +6,9 @@ $nome = $conn->real_escape_string($_REQUEST['nome']);
 $cognome = $conn->real_escape_string($_REQUEST['cognome']);
 $email = $conn->real_escape_string($_REQUEST['email']);
 $password = $conn->real_escape_string($_REQUEST['password']);
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$sql = "INSERT INTO utenti (nome, cognome, email, password) VALUES ('$nome', '$cognome', '$email', '$password')";
+$sql = "INSERT INTO utenti (nome, cognome, email, password) VALUES ('$nome', '$cognome', '$email', '$hashed_password')";
 
 if($conn->query($sql) === true){
     echo "Registrazione effettuata con successo";
